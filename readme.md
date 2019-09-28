44b7ba09d9a4a3325c82dc9b0f7ed50d0656db63
LUTFAIA <30468948+LUTFAIA@users.noreply.github.com>
2019-09-28 20:22:42 +0700

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,1 @@
+Kegunaan JSON pada REST API yaitu sebagai format untuk mentransmisikan (mengirim dan menerima) data dari web API (server) ke client atau sebaliknya.
---
+++ soal 5.php
@@ -0,0 +1,9 @@
+<?php
+$string = "Luthfi Fadhlurrahman Aulia";
+if ($result = substr_count($string, "a")) {
+    echo "Jumlah Huruf Yaitu $result";
+}
+else {
+    echo "Not Found";
+}
+?>
